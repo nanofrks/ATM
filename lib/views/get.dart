f964542503344aa215e823fb97538a3ed2f3dd6c
nanofrks <softivyad@gmail.com>
2023-04-30 23:36:52 -0500

    Obtener gatitos

--- lib/views/get.dart
+++ lib/views/get.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login_view.dart';
+import 'package:atm_ingsoftware/views/home_view.dart';
 
 class GetView extends StatelessWidget{
   const GetView ({super.key});
@@ -29,20 +29,161 @@ class GetView extends StatelessWidget{
           ListView( //todo el contenido que está en pantalla
               children: <Widget>[
                 Container(
-                  padding: const EdgeInsets.only(left: 128, right: 128),
+                  padding: const EdgeInsets.only(left: 40, right: 40, top: 40, bottom: 40),
                   margin: const EdgeInsets.all(45),
                   height: MediaQuery.of(context).size.height/1.1,
                   color: const Color.fromRGBO(0, 28, 66, 1),
                   child: Row(
                     children: <Widget>[
-                      ElevatedButton(
-                        onPressed:  () {
-                          Navigator.push(context, MaterialPageRoute(builder: (_)=> const LoginView()));
-                        }, 
-                        child: const Text(
-                          'Volver',
-                        )
+                      SizedBox(
+                        child: Column(
+                          children: <Widget>[
+                            ElevatedButton(
+                              onPressed:  () {
+                                Navigator.push(context, MaterialPageRoute(builder: (_)=> const Homeview()));
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color.fromRGBO(120, 67, 230,1),
+                                  fixedSize: const Size(150, 50),
+                                  ),
+                              child: const Text(
+                                'Volver',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(255, 187, 234, 1),
+                                  fontFamily: 'RetroGaming',
+                                  fontSize: 24,
+                                )
+                              )
+                              ),
+                              
+                          ],
                         ),
+                      ),
+                      const SizedBox(width: 50),
+                      const SizedBox(height: 20),
+                      const Image(
+                        image: AssetImage('assets/images/gacha.png'),
+                        fit: BoxFit.scaleDown,
+                      ),
+                      const SizedBox(width: 50),
+
+                      SizedBox(
+                        child: Column(
+                          children: <Widget>[
+                            const SizedBox(height: 100),
+                            ElevatedButton(
+                              onPressed:  () {
+                                showDialog(context: context,
+                                  builder: (BuildContext context){
+                                    return Dialog(
+                                      backgroundColor: const Color.fromRGBO(120, 67, 230,1),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20.0)),
+                                        child: SizedBox(
+                                          height: 200,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(12.0),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              children: [
+                                                const Text(
+                                                  'Canjeando...',
+                                                  style: TextStyle(
+                                                    color: Color.fromRGBO(255, 187, 234, 1),
+                                                    fontFamily: 'RetroGaming',
+                                                    fontSize: 17,
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 20),
+                                                CircularProgressIndicator(
+                                                  valueColor:
+                                                  AlwaysStoppedAnimation( Colors.blue.shade300),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                    );
+                                    }
+                                  );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color.fromRGBO(120, 67, 230,1),
+                                  fixedSize: const Size(150, 100),
+                                  ),
+                              child: const Text(
+                                'usar\nmonedas',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(255, 187, 234, 1),
+                                  fontFamily: 'RetroGaming',
+                                  fontSize: 18,
+                                )
+                              )
+                              ),
+
+                              const SizedBox(height: 50),
+
+                              ElevatedButton(
+                              onPressed:  () {
+                                showDialog(context: context,
+                                  builder: (BuildContext context){
+                                    return Dialog(
+                                      backgroundColor: const Color.fromRGBO(120, 67, 230,1),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20.0)),
+                                        child: SizedBox(
+                                          height: 200,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(12.0),
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              crossAxisAlignment: CrossAxisAlignment.center,
+                                              children: [
+                                                const Text(
+                                                  'Canjeando...',
+                                                  style: TextStyle(
+                                                    color: Color.fromRGBO(255, 187, 234, 1),
+                                                    fontFamily: 'RetroGaming',
+                                                    fontSize: 17,
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 20),
+                                                CircularProgressIndicator(
+                                                  valueColor:
+                                                  AlwaysStoppedAnimation( Colors.blue.shade300),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                    );
+                                    }
+                                  );
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color.fromRGBO(120, 67, 230,1),
+                                  fixedSize: const Size(150, 100),
+                                  ),
+                              child: const Text(
+                                'leer\nQR',
+                                style: TextStyle(
+                                  color: Color.fromRGBO(255, 187, 234, 1),
+                                  fontFamily: 'RetroGaming',
+                                  fontSize: 18,
+                                )
+                              )
+                              ),
+                              
+                          ],
+                        ),
+                      ),
+
+                      
+                      
+
+
+
                     ],
                   )
                 )
